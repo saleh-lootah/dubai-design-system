eddca14f34459fa4e10209050870cad136f49f8f
Saleh <salehtl@icloud.com>
2026-07-15 15:29:52 +0000

    test(samples): vue sample proves components-vue works from npm

---
+++ samples/vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { DdaButton } from '@dubai-design-system/components-vue';
+</script>
+
+<template>
+  <div>
+    <h1>DDA Vue sample</h1>
+    <DdaButton
+      button_color="default-primary"
+      start_icon="sentiment_satisfied"
+      end_icon="arrow_forward"
+      button_id="button"
+      aria_label="button"
+    >Button</DdaButton>
+  </div>
+</template>
---
+++ samples/vue/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import '@dubai-design-system/components-js/dist/components/dda-button.js';
+import App from './App.vue';
+
+createApp(App).mount('#app');
